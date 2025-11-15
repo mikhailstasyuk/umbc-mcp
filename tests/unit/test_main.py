bec988bb9ed8dbead8f8ce1b9627ba2a73249d75
Mikhail Stasyuk <mikhail.stasyuk@gmail.com>
2025-11-15 13:04:01 +0400

    fix: fixed the name of the  health test

--- tests/unit/test_main.py
+++ tests/unit/test_main.py
@@ -4,7 +4,7 @@ from src.app.main import app
 client = TestClient(app)
 
 
-def test_root():
+def test_health():
     response = client.get("/health")
     assert response.status_code == 200
     assert response.json() == {"status": "healthy"}
